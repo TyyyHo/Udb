8996ed105be4e44bbfb58ea8ce463d33f8622cb2
TyyyHo <ty.ho1126@gmail.com>
2023-02-01 18:45:00 +0800

    first commit

--- past ver/ver0.2/src/page/rune/rune_box/link_data/link_output_data.js
+++
@@ -1,5 +0,0 @@
-import { red_link } from "./red_link";
-import { green_link } from "./green_link";
-import { blue_link } from "./blue_link";
-
-export const link_output_data = red_link.concat(green_link).concat(blue_link);
--- past ver/ver0.2/src/page/rune/rune_box/skill_data/combined_data_skill.js
+++
@@ -1,5 +0,0 @@
-import { red_skill } from "./red_skill";
-import { green_skill } from "./green_skill";
-import { blue_skill } from "./blue_skill";
-
-export const combined_data_skill = red_skill.concat(green_skill).concat(blue_skill);
